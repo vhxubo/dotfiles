f56791b0304d3b578047c7be460764a16451ecf8
vhxubo <vhxubo@gmail.com>
2023-04-22 21:30:58 +0800

    chore: update lazyvim config

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -14,9 +14,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "html",
-        "typescript",
         "fish",
         "vue",
         "css",
