e5388baf240a8d46f3d6dbeb13d898a09bd757b3
vhxubo <vhxubo@gmail.com>
2023-04-26 23:45:14 +0800

    chore: update fish shell config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,24 +2,25 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias nvim="lvim"
+# alias nvim="lvim"
 alias pn="pnpm"
 alias pd="pnpm dev"
-alias n="lvim ."
+alias n="nvim ."
 alias lgit="lazygit"
 alias t="tmux"
-alias tt="t attach -t"
+alias ta="t attach"
 alias ts="t new -s"
-alias close="shutdown -h now"
+alias tt="t attach -t"
 
-set -gx EDITOR lvim
+set -gx EDITOR nvim
+set fzf_fd_opts --hidden --exclude=.git
 
 function fish_proxy
-  set -xg ALL_PROXY http://localhost:7890
+    set -xg ALL_PROXY http://localhost:7890
 end
 
 function noproxy
-  set -e ALL_PROXY
+    set -e ALL_PROXY
 end
 
 zoxide init fish | source
