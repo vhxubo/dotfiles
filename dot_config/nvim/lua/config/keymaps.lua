39f8aa6424ff9377599875861725ab353343f072
vhxubo <vhxubo@gmail.com>
2023-10-26 09:54:24 +0800

    chore!: clear old nvim config

--- dot_config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- https://vimtricks.com/p/get-the-current-file-path/
-vim.keymap.set("n", "<leader>yc", ':let @+=fnamemodify(expand("%:f"), ":~:.") . ":" . line(".")<CR>')
