d77ef35e61426b2eba88b92e9310ff51fb326d43
vhxubo <vhxubo@gmail.com>
2023-11-09 17:15:00 +0800

    chore: update nvim config

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -14,4 +14,13 @@ return {
       "L3MON4D3/LuaSnip",
     },
   },
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup()
+    end,
+  },
 }
