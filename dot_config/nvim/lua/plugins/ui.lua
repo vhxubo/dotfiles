3ed6ff4699ccd71af7bc6344d7d3ffbd75fa10c7
vhxubo <vhxubo@gmail.com>
2023-11-16 14:36:25 +0800

    chore(nvim): change nvim-srollview to satellite.nvim

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -8,19 +8,9 @@ return {
     },
   },
   {
-    "dstein64/nvim-scrollview",
-    config = function()
-      require("scrollview").setup({
-        excluded_filetypes = { "nerdtree", "help" },
-        current_only = true,
-        winblend = 75,
-        base = "buffer",
-        column = 120,
-        signs_on_startup = { "all" },
-        diagnostics_severities = { vim.diagnostic.severity.ERROR },
-      })
-      require("scrollview.contrib.gitsigns").setup()
-    end,
+
+    "lewis6991/satellite.nvim",
+    config = true,
   },
   {
     "3rd/image.nvim",
