e8810d18c684f8ef412f81fbcd6df1dd31c31ed7
vhxubo <vhxubo@gmail.com>
2023-11-19 16:25:25 +0800

    chore(nvim): replace fzf.vim with fzf-lua

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -1,6 +1,10 @@
 return {
   {
-    { "junegunn/fzf", build = "./install --bin" },
-    { "junegunn/fzf.vim" },
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("fzf-lua").setup({ "telescope" })
+      vim.keymap.set("n", "<C-p>", "<cmd>lua require('fzf-lua').files()<cr>", { silent = true })
+    end,
   },
 }
