69dc11237839f1c2dfdb0f8329968fe606b29a47
vhxubo <vhxubo@gmail.com>
2023-11-23 19:00:59 +0800

    perf(nvim): change bufferline mode to tab, don't use buffer mode, because it make so many "tab"

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -49,6 +49,22 @@ return {
       })
     end,
   },
+  -- buffer line
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
   -- filename
   {
     "b0o/incline.nvim",
