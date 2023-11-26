387afb074fc62d781bf5b9b03aabbb2e58a4fbcd
vhxubo <vhxubo@gmail.com>
2023-11-26 23:16:15 +0800

    feat(nvim): add telescope-undo.nvim plugin

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -111,4 +111,27 @@ return {
       require("telescope").load_extension("file_browser")
     end,
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "debugloop/telescope-undo.nvim",
+    },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          undo = {
+            theme = "dropdown",
+            side_by_side = true,
+            layout_strategy = "vertical",
+            layout_config = {
+              preview_height = 0.8,
+            },
+          },
+        },
+      })
+      require("telescope").load_extension("undo")
+      vim.keymap.set("n", "<leader>su", "<cmd>Telescope undo<cr>")
+    end,
+  },
 }
