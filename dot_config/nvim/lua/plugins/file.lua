2dfb567e22e68846179b81a3fbcf1ec848b261c4
vhxubo <vhxubo@gmail.com>
2023-11-26 22:17:56 +0800

    feat(nvim): replace mini.file with oil.nvim

--- dot_config/nvim/lua/plugins/file.lua
+++ dot_config/nvim/lua/plugins/file.lua
@@ -7,4 +7,13 @@ return {
       vim.keymap.set("n", "<C-p>", "<cmd>lua require('fzf-lua').files()<cr>", { silent = true })
     end,
   },
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function(_, opts)
+      require("oil").setup(opts)
+      vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+    end,
+  },
 }
