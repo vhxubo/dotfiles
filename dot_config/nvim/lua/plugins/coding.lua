24c200860a0e6530c8fc0bb97977ae6ec0595e53
vhxubo <vhxubo@gmail.com>
2023-12-06 18:13:29 +0800

    chore(nvim): remove mini.bracketed plugin

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -55,22 +55,6 @@ return {
     opts = {},
   },
 
-  -- Go forward/backward with square brackets
-  {
-    "echasnovski/mini.bracketed",
-    event = "BufReadPost",
-    config = function()
-      local bracketed = require("mini.bracketed")
-      bracketed.setup({
-        file = { suffix = "" },
-        window = { suffix = "" },
-        quickfix = { suffix = "" },
-        yank = { suffix = "" },
-        treesitter = { suffix = "n" },
-      })
-    end,
-  },
-
   -- Better increase/descrease
   {
     "monaqa/dial.nvim",
