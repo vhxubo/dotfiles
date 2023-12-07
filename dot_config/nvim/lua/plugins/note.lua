09fb4b133282e13cc908390e7666f3447b91f50d
vhxubo <vhxubo@gmail.com>
2023-12-07 21:28:05 +0800

    chore(nvim): remove telekasten.nvim dependencies, no more need

--- dot_config/nvim/lua/plugins/note.lua
+++ dot_config/nvim/lua/plugins/note.lua
@@ -1,10 +1,6 @@
 return {
   {
     "renerocksai/telekasten.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "renerocksai/calendar-vim",
-    },
     config = function()
       require("telekasten").setup({
         home = vim.fn.expand("~/Nutstore Files/notes/0-zettelkasten"),
