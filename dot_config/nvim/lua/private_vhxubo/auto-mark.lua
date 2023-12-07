b27cf87c4a336aae663b01d905561be13e0e0254
vhxubo <vhxubo@gmail.com>
2023-12-07 13:29:54 +0800

    fix(nvim): auto-mark can't mark mutiple line return

--- dot_config/nvim/lua/private_vhxubo/auto-mark.lua
+++ dot_config/nvim/lua/private_vhxubo/auto-mark.lua
@@ -18,7 +18,8 @@ local function set_vue_marks()
     vim.api.nvim_buf_set_mark(bufrn, "s", s_row, s_col, {})
   end
 
-  local r_row, r_col = unpack(vim.fn.searchpos("return {.*}$", "nbce"))
+  -- https://vim.fandom.com/wiki/Search_across_multiple_lines#Searching_for_multiline_HTML_comments
+  local r_row, r_col = unpack(vim.fn.searchpos([[return {\_.\{-}}]], "nbce"))
   if r_row ~= 0 or r_col ~= 0 then
     vim.api.nvim_buf_set_mark(bufrn, "r", r_row, r_col - 1, {})
   end
