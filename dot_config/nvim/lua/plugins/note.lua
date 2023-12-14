53c21f3cd7d86cb9a077a1b183dd44483dd9197b
vhxubo <vhxubo@gmail.com>
2023-12-14 18:08:31 +0800

    feat(nvim): add zk-nvim for note text

--- dot_config/nvim/lua/plugins/note.lua
+++ dot_config/nvim/lua/plugins/note.lua
@@ -19,31 +19,28 @@ return {
     end,
   },
   {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    "mickael-menu/zk-nvim",
     config = function()
-      require("neorg").setup({
-        load = {
-          ["core.defaults"] = {},
-          ["core.keybinds"] = {},
-          ["core.concealer"] = {},
-          ["core.qol.todo_items"] = {},
-          ["core.summary"] = {},
-          ["core.integrations.treesitter"] = {},
-          ["core.dirman"] = {
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-              default_workspace = "notes",
-            },
-          },
-        },
-      })
+      require("zk").setup({})
+      local opts = { noremap = true, silent = false }
 
-      vim.wo.foldlevel = 99
-      vim.wo.conceallevel = 2
+      -- Create a new note after asking for its title.
+      vim.api.nvim_set_keymap("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", opts)
+
+      -- Open notes.
+      vim.api.nvim_set_keymap("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
+      -- Open notes associated with the selected tags.
+      vim.api.nvim_set_keymap("n", "<leader>zt", "<Cmd>ZkTags<CR>", opts)
+
+      -- Search for the notes matching a given query.
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>zf",
+        "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>",
+        opts
+      )
+      -- Search for the notes matching the current visual selection.
+      vim.api.nvim_set_keymap("v", "<leader>zf", ":'<,'>ZkMatch<CR>", opts)
     end,
   },
 }
