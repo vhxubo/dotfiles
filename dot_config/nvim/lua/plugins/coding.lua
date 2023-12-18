2d8afebd10e68143fbe7aed467515634c0826eb7
vhxubo <vhxubo@gmail.com>
2023-12-19 00:23:32 +0800

    feat(nvim): add nvim-html-css plugin

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -146,4 +146,33 @@ return {
     },
   },
   { "tpope/vim-rsi" },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      table.insert(opts.sources, {
+        name = "html-css",
+        option = {
+          max_count = 3,
+          enable_on = {
+            "html",
+            "vue",
+          },
+          file_extensions = { "css", "sass", "less" },
+          style_sheets = {
+            "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "Jezda1337/nvim-html-css",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("html-css"):setup()
+    end,
+  },
 }
