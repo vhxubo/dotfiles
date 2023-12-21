480a367a5bfcc51e2e7eba41e7d1d6bb3a740a85
vhxubo <vhxubo@gmail.com>
2023-12-21 13:24:51 +0800

    feat(nvim): add nvim-spider improve default w,e,b

--- dot_config/nvim/lua/plugins/navigate.lua
+++ dot_config/nvim/lua/plugins/navigate.lua
@@ -71,4 +71,26 @@ return {
       dim = true, -- dim all other characters if set to true (recommended!)
     },
   },
+  { "gbprod/stay-in-place.nvim", config = true },
+  { "nacro90/numb.nvim", config = true },
+  {
+    "chrisgrieser/nvim-spider",
+    keys = {
+      {
+        "w",
+        "<cmd>lua require('spider').motion('w')<CR>",
+        { desc = "Spider-w" },
+      },
+      {
+        "e",
+        "<cmd>lua require('spider').motion('e')<CR>",
+        { desc = "Spider-e" },
+      },
+      {
+        "b",
+        "<cmd>lua require('spider').motion('b')<CR>",
+        { desc = "Spider-b" },
+      },
+    },
+  },
 }
