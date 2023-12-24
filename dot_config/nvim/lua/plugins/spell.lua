c27541cd4d72026fd877922d1c9753fdd3c3207c
vhxubo <vhxubo@gmail.com>
2023-12-24 19:43:30 +0800

    chore(nvim): disable spelunker.vim, need use keymap toggle

--- dot_config/nvim/lua/plugins/spell.lua
+++ dot_config/nvim/lua/plugins/spell.lua
@@ -5,6 +5,9 @@ return {
     config = function()
       vim.api.nvim_set_hl(0, "SpelunkerSpellBad", { underdotted = 1 })
       vim.api.nvim_set_hl(0, "SpelunkerComplexOrCompoundWord", { underdotted = 1 })
+      -- ZT: Toggle to enable or disable.
+      -- Zt: Toggle to enable or disable only the current buffer.
+      vim.g.enable_spelunker_vim = 0
       vim.g.spelunker_check_type = 2
     end,
   },
