2db73ac1ec80c1259998080204d1885abf5ccff0
vhxubo <vhxubo@gmail.com>
2024-01-06 10:47:44 +0800

    feat(nvim): config spellcheck lsp

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -15,4 +15,11 @@ vim.opt.laststatus = 3
 -- -- To prevent this, set `splitkeep` to either `screen` or `topline`.
 vim.opt.splitkeep = "screen"
 
+vim.opt.spell = true
 vim.opt.spelloptions = "camel"
+vim.opt.spelllang = { "en_us" }
+
+-- https://jaketrent.com/post/set-node-version-nvim/
+-- required: fnm install v20.10.0
+local node_bin = "/home/vhxubo/.local/share/fnm/node-versions/v20.10.0/installation/bin"
+vim.cmd("let $PATH = '" .. node_bin .. ":' . $PATH")
--- dot_config/nvim/lua/plugins/spell.lua
+++ dot_config/nvim/lua/plugins/spell.lua
@@ -1,14 +1,25 @@
 return {
   {
-    "kamykn/spelunker.vim",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      vim.api.nvim_set_hl(0, "SpelunkerSpellBad", { underdotted = 1 })
-      vim.api.nvim_set_hl(0, "SpelunkerComplexOrCompoundWord", { underdotted = 1 })
-      -- ZT: Toggle to enable or disable.
-      -- Zt: Toggle to enable or disable only the current buffer.
-      vim.g.enable_spelunker_vim = 0
-      vim.g.spelunker_check_type = 2
+    "hrsh7th/nvim-cmp",
+    dependencies = { "f3fora/cmp-spell" },
+    opts = function(_, opts)
+      vim.list_extend(opts.sources, { { name = "spell" } })
     end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "cspell" })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        -- Use the "*" filetype to run formatters on all filetypes.
+        ["*"] = { "cspell" },
+      },
+    },
   },
 }
