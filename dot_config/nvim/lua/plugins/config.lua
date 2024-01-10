2247183fe58ef3223fcbd8fc529f6c7b72e60822
vhxubo <vhxubo@gmail.com>
2024-01-10 16:55:31 +0800

    feat(nvim): config copilot for auto suggestion with cmp

--- dot_config/nvim/lua/plugins/config.lua
+++ dot_config/nvim/lua/plugins/config.lua
@@ -128,8 +128,7 @@ return {
             -- fallback()
             require("neotab").tabout()
           else
-            require("neotab").tabout()
-            -- fallback()
+            fallback()
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
@@ -142,6 +141,22 @@ return {
           end
         end, { "i", "s" }),
       })
+      cmp.event:on("menu_opened", function()
+        vim.b.copilot_suggestion_hidden = true
+      end)
+
+      cmp.event:on("menu_closed", function()
+        vim.b.copilot_suggestion_hidden = false
+      end)
     end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+      },
+    },
   },
 }
