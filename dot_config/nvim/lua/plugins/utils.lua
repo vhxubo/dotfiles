4ad9f6c4ffc326cdf8c69ca3e937a794d7cb968d
vhxubo <vhxubo@gmail.com>
2024-01-10 16:56:10 +0800

    feat(nvim): support fzy wilder.nvim

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -27,16 +27,43 @@ return {
   },
   {
     "gelguy/wilder.nvim",
+    dependencies = {
+      "romgrk/fzy-lua-native",
+    },
     config = function()
       local wilder = require("wilder")
       wilder.setup({ modes = { ":" } })
+      -- Disable Python remote plugin
+      wilder.set_option("use_python_remote_plugin", 0)
+
+      wilder.set_option("pipeline", {
+        wilder.branch(
+          wilder.cmdline_pipeline({
+            fuzzy = 1,
+            fuzzy_filter = wilder.lua_fzy_filter(),
+          }),
+          wilder.vim_search_pipeline()
+        ),
+      })
+
       wilder.set_option(
         "renderer",
-        wilder.popupmenu_renderer({
-          pumblend = 20,
-          min_width = "100%", -- minimum height of the popupmenu, can also be a number
-          min_height = "50%", -- to set a fixed height, set max_height to the same value
-          reverse = 0, -- if 1, shows the candidates from bottom to top
+        wilder.renderer_mux({
+          [":"] = wilder.popupmenu_renderer({
+            pumblend = 20,
+            min_width = "100%", -- minimum height of the popupmenu, can also be a number
+            min_height = "50%", -- to set a fixed height, set max_height to the same value
+            reverse = 0, -- if 1, shows the candidates from bottom to top
+            highlighter = wilder.lua_fzy_highlighter(),
+            left = {
+              " ",
+              wilder.popupmenu_devicons(),
+            },
+            right = {
+              " ",
+              wilder.popupmenu_scrollbar(),
+            },
+          }),
         })
       )
     end,
@@ -48,6 +75,10 @@ return {
   },
   {
     "stevearc/overseer.nvim",
-    opts = {},
+    config = function()
+      require("overseer").setup({})
+      vim.keymap.set("n", "<leader>re", "<cmd>OverseerToggle<CR>", { desc = "Overseer explorer" })
+      vim.keymap.set("n", "<leader>rr", "<cmd>OverseerRun<CR>", { desc = "Overseer run task" })
+    end,
   },
 }
