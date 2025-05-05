9f713d09492df6c0b5be9fb17f1e9e5e272f653d
vhxubo <vhxubo@gmail.com>
2025-05-05 08:07:14 +0800

    chore: update neovim config

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -5,16 +5,4 @@ return {
       vim.keymap.set("n", "<leader>G", "<cmd>G<cr>")
     end,
   },
-  {
-    "akinsho/git-conflict.nvim",
-    version = "*",
-    config = function()
-      require("git-conflict").setup({
-        default_mappings = {
-          next = "]g",
-          prev = "[g",
-        },
-      })
-    end,
-  },
 }
