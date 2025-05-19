74a07eee68e76ebce12e97a0aec05a6960475473
vhxubo <vhxubo@gmail.com>
2025-05-19 15:34:18 +0800

    chore: update neovim config

--- dot_config/nvim/lua/plugins/config.lua
+++ dot_config/nvim/lua/plugins/config.lua
@@ -14,6 +14,28 @@ return {
       window = {
         position = "right",
       },
+      filesystem = {
+        window = {
+          mappings = {
+            ["O"] = "system_open",
+          },
+        },
+      },
+      commands = {
+        system_open = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          -- Windows: Without removing the file from the path, it opens in code.exe instead of explorer.exe
+          local p
+          local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- Match the last slash and everything before it
+          if lastSlashIndex then
+            p = path:sub(1, lastSlashIndex - 1) -- Extract substring before the last slash
+          else
+            p = path -- If no slash found, return original path
+          end
+          vim.cmd("silent !start explorer " .. p)
+        end,
+      },
     },
   },
 }
